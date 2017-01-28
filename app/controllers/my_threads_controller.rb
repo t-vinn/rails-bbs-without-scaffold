3a7c89da49bb7b211e6a24f15ac878bc392e903a
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-01-28 19:13:30 +0900

    delete and edit functions for threads

--- app/controllers/my_threads_controller.rb
+++ app/controllers/my_threads_controller.rb
@@ -21,4 +21,25 @@ class MyThreadsController < ApplicationController
     end
   end
 
+  def edit
+    @my_thread = MyThread.find(params[:id])
+  end
+
+  def update
+    @my_thread = MyThread.find(params[:id])
+    if @my_thread.update(params.require(:my_thread).permit(:title))
+      redirect_to @my_thread, notice: 'the thread name changed'
+    else
+      flash.now[:alert] = "no title"
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @my_thread = MyThread.find(params[:id])
+    @my_thread.destroy
+    flash.now[:notice] = "a thread correctly deleted"
+  end
+
 end
