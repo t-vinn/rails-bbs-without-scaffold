94344c80965784470097fbcc46d997892e7df2f5
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-01-28 17:43:00 +0900

    creating new thread feature q

--- app/controllers/my_threads_controller.rb
+++ app/controllers/my_threads_controller.rb
@@ -6,4 +6,18 @@ class MyThreadsController < ApplicationController
   def show
   end
 
+  def new
+    @my_thread = MyThread.new
+  end
+
+  def create
+    @my_thread = MyThread.new(params.require(:my_thread).permit(:title))
+    if @my_thread.save
+      redirect_to @my_thread, notice: 'a new thread created'
+    else
+      flash.now[:alert] = "no title"
+      render :new
+    end
+  end
+
 end
--- app/models/my_thread.rb
+++ app/models/my_thread.rb
@@ -1,2 +1,3 @@
 class MyThread < ApplicationRecord
+  validates :title, presence: true
 end
